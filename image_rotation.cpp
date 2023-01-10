19d4b06d4f150d5642abf3d1ecc9a729abdb53a2
KomarovVladimir <komarov2142@gmail.com>
2023-01-11 00:03:10 +0300

    update

--- image_rotation.cpp
+++ image_rotation.cpp
@@ -1,33 +1,95 @@
 #include <iostream>
-#include <algorithm>
-#include "bmpmini.hpp"
+#include <fstream>
+#include <string>
+#include <Windows.h>
+
+bool rotate(char* src, char* dst, BITMAPINFOHEADER& bi, int angle)
+{
+	//In 24bit image, the length of each row must be multiple of 4
+	int padw = 4 - ((bi.biWidth * 3) % 4);
+	if (padw == 4) padw = 0;
+
+	int padh = 4 - ((bi.biHeight * 3) % 4);
+	if (padh == 4) padh = 0;
+
+	int pad2 = 0;
+	if (padh == 1 || padh == 3) pad2 = 2;
+
+	bi.biHeight += padh;
+
+	int w = bi.biWidth;
+	int h = bi.biHeight;
+	if (angle == 90 || angle == 270)
+	{
+		std::swap(bi.biWidth, bi.biHeight);
+	}
+	else
+	{
+		bi.biHeight -= padh;
+	}
+
+	for (int row = 0; row < h; row++)
+	{
+		for (int col = 0; col < w; col++)
+		{
+			int n1 = 3 * (col + w * row) + padw * row;
+			int n2 = 0;
+
+			switch (angle)
+			{
+			case 0:   n2 = 3 * (col + w * row) + padw * row; break;
+			case 90:  n2 = 3 * ((h - row - 1) + h * col) + pad2 * col; break;
+			case 180: n2 = 3 * (col + w * (h - row - 1)) + padw * (h - row - 1); break;
+			case 270: n2 = 3 * (row + h * col) + pad2 * col; break;
+			}
+
+			dst[n2 + 0] = src[n1 + 0];
+			dst[n2 + 1] = src[n1 + 1];
+			dst[n2 + 2] = src[n1 + 2];
+		}
+	}
+
+	for (int row = 0; row < bi.biHeight; row++)
+		for (int col = 0; col < padw; col++)
+			dst[bi.biWidth * 3 + col] = 0;
+
+	bi.biSizeImage = (bi.biWidth + padw) * bi.biHeight * 3;
+
+	return true;
+}
 
 int main()
 {
-	using namespace image;
-	BMPMini bmp;
+	std::string input = "samples/lena_color.bmp";
+	std::string output = "results/lena_color_rotated.bmp";
 
-	try {
-		bmp.read("./samples/image.bmp");
-		bmp.write("./results/image_copy.bmp");
-		auto img = bmp.get();
-		img.channels = 1;
-		std::vector<uint8_t> gray(img.width * img.height);
-		struct BGR {
-			uint8_t b{ 0 };
-			uint8_t g{ 0 };
-			uint8_t r{ 0 };
-		};
-		BGR* color = reinterpret_cast<BGR*>(img.data);
-		std::transform(color, color + gray.size(), gray.begin(),
-			[](const BGR& c) { return (uint8_t)std::min(std::max(0.299 * c.r + 0.587 * c.g + 0.114 * c.b, 0.0), 255.0); });
-		img.data = gray.data();
-		bmp.write(img, "./results/image_gray.bmp");
+	BITMAPFILEHEADER bf = { 0 };
+	BITMAPINFOHEADER bi = { sizeof(BITMAPINFOHEADER) };
+
+	std::ifstream fin(input, std::ios::binary);
+	if (!fin) return 0;
+
+	fin.read((char*)&bf, sizeof(bf));
+	fin.read((char*)&bi, sizeof(bi));
+
+	int size = 3 * (bi.biWidth + 3) * (bi.biHeight + 3);
+	char* src = new char[size];
+	char* dst = new char[size];
+
+	fin.read(src, bi.biSizeImage);
+
+	//use 0, 90, 180, or 270 for the angle
+	if (rotate(src, dst, bi, 90))
+	{
+		bf.bfSize = 54 + bi.biSizeImage;
+		std::ofstream fout(output, std::ios::binary);
+		fout.write((char*)&bf, 14);
+		fout.write((char*)&bi, 40);
+		fout.write((char*)dst, bi.biSizeImage);
 	}
-	catch (const std::exception& e) {
-		std::cout << e.what() << std::endl;
-		return -1;
-	}
+
+	delete[]src;
+	delete[]dst;
 
 	return 0;
 }
